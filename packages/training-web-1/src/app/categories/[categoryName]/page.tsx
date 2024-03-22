2f19cd3563bef8b6584b8d959a7109c1b41b374c
neko-no <101441574+neko-no@users.noreply.github.com>
2024-03-22 18:54:07 +0900

    add: useRouterを用いたルーティングの追加

--- packages/training-web-1/src/app/categories/[categoryName]/page.tsx
+++ packages/training-web-1/src/app/categories/[categoryName]/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 type Props = {
   params: { categoryName: string };
   searchParams: { [key: string]: string | string[] | undefined };
@@ -6,11 +10,20 @@ type Props = {
 // ★:props からパスパラメーター、URL 検索パラメーターが参照できる
 export default function Page({ params, searchParams }: Props) {
   const page = typeof searchParams.page === "string" ? searchParams.page : "1";
+  const router = useRouter();
+
   return (
     <div>
       <h1>カテゴリー別一覧画面</h1>
       <h2>カテゴリー「{params.categoryName}」</h2>
       <p>ページ番号：「{page}」</p>
+      <button
+        onClick={() => {
+          router.push("/categories");
+        }}
+      >
+        戻る
+      </button>
     </div>
   );
 }
